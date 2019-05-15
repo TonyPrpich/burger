300fcf5f9e1bbc23b150bd7a546fc906511ee9de
ArthurM0rgan <46456968+TonyPrpich@users.noreply.github.com>
2019-05-14 22:31:48 -0600

    updated seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("LE Big Mac", false, "2019-05-14 04:20:00"),
+  ("Plain Cheese Burger", false, "2019-05-14 16:20:00"),
+  ("A Royale W Cheese", false, "2019-05-14 16:20:01"),
+  ("Garlic Burger", false, "2019-05-14 04:20:20"),
+  ("3x3 w/0 onions", true, "2019-05-14 16:20:16");
