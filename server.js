e87f0e7c41a920491f6dab04e8330e0b2eabcce3
ArthurM0rgan <46456968+TonyPrpich@users.noreply.github.com>
2019-05-14 22:23:50 -0600

    server.js commit

---
+++ server.js
@@ -0,0 +1,24 @@
+
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var exphbs = require("express-handlebars");
+
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.use(express.static('public'));
+
+
+app.use(require('./controllers/burgers_controller'));
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
